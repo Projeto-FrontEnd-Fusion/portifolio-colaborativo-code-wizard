4dc1566a818543fc494d73b6040d69ec52095d48
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-05-02 23:07:14 -0300

    FEAT : "adicionado Tailwind css, Variáveis de estilo, e pagina de Components"

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens : {
+      "mini-screen" : '430px',
+      "small-screen": '480px',
+      "medium-screen": '768px',
+      "larger-screen": '976px',
+      'super-screen' : "1024",
+      "mega-screen" : "1280"
+    },
+    colors : {
+      "dark-color" : "#0F0920", 
+      "dark-color-second" : "#221C3E",
+      "light-color" : "#f1f1f1",
+      "primary-button" : "#16C586",
+      "light-color-text1" : "#F2F0FF",
+      "light-color-text2" : "#B5B3BC",
+
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
