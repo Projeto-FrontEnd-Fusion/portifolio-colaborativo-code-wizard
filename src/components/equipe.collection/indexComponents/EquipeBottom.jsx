0be6f507b3f6234dc095d8f473fbd54197454d3a
Filipe Dias <diasjoaum@gmail.com>
2024-05-23 15:21:51 -0300

    Melhoria de vida

--- src/components/equipe.collection/indexComponents/EquipeBottom.jsx
+++ src/components/equipe.collection/indexComponents/EquipeBottom.jsx
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import projectDefault from "../imageProject/projectDefault.png";
 
 export default function EquipeBottom(User) {
     const [showProject, setShowProject] = useState(false)
@@ -32,38 +33,56 @@ export default function EquipeBottom(User) {
             <menu
                 className={`flex flex-wrap justify-center w-full h-fit gap-2 larger-screen:justify-start larger-screen:gap-8 larger-screen:mb-8`}>
                 <button onClick={toggleProjects}
-                        className={`w-40 p-2 rounded bg-primary-button text-light-color font-inconsolata cursor-pointer duration-150 hover:bg-dark-color-second dark:hover:bg-primary-button larger-screen:hover:bg-primary-button larger-screen:w-48 larger-screen:bg-dark-color dark:bg-dark-color-second`}>
+                        className={`w-40 p-2 rounded bg-primary-button text-light-color font-inconsolata cursor-pointer duration-150 hover:bg-dark-color-second dark:hover:bg-primary-button larger-screen:hover:bg-primary-button larger-screen:w-48 larger-screen:bg-[#111111] dark:bg-dark-color-second`}>
                     Projetos Recentes
                 </button>
                 <button onClick={toggleSkills}
-                        className={`w-40 p-2 rounded bg-primary-button text-light-color font-inconsolata cursor-pointer duration-150 hover:bg-dark-color-second dark:hover:bg-primary-button larger-screen:hover:bg-primary-button larger-screen:w-48 larger-screen:bg-dark-color dark:bg-dark-color-second`}>
+                        className={`w-40 p-2 rounded bg-primary-button text-light-color font-inconsolata cursor-pointer duration-150 hover:bg-dark-color-second dark:hover:bg-primary-button larger-screen:hover:bg-primary-button larger-screen:w-48 larger-screen:bg-[#111111] dark:bg-dark-color-second`}>
                     Habilidades
                 </button>
             </menu>
             <div className={`grid w-full gap-4 overflow-hidden duration-200 ${showProject ? "open" : "close"}`}>
                 <div
                     className={`flex flex-col items-center w-full h-fit gap-4 overflow-hidden larger-screen:flex-row larger-screen:flex-wrap larger-screen:gap-8`}>
-                    {info && info.projects.map((project, id) => {
-                        return (
+                    {info && info.projects.length === 0 ? (
+                        <div
+                            className="flex items-center justify-center flex-col gap-2 w-full h-fit larger-screen:items-start">
+                            <p className={`font-inconsolata font-bold text-center dark:text-light-color text-3xl`}>
+                                Nenhum projeto adicionado.
+                            </p>
+                        </div>
+                    ) : (
+                        info && info.projects.map((project, id) => (
                             <picture key={id}
-                                     className={`overflow-hidden flex items-center justify-center w-52 h-36 bg-dark-color rounded-lg cursor-pointer larger-screen:w-[300px] larger-screen:h-[210px] dark:bg-light-color`}>
-                                <img src="./vite.svg" alt="image"
-                                     className={`size-20 object-cover larger-screen:size-40 select-none`}/>
+                                     className={`group overflow-hidden flex items-center justify-center w-52 h-36 bg-dark-color-second rounded-lg cursor-pointer larger-screen:w-[300px] larger-screen:h-[210px]`}>
+                                <a href={project.link} target="_blank"
+                                   className={`relative size-full flex items-center justify-center`}>
+                                    <img src={projectDefault} alt={project.name}
+                                         className={`size-full object-cover duration-150 group-hover:sepia-0 sepia`}/>
+                                    <div
+                                        className={`absolute size-full flex items-center justify-center backdrop-brightness-50`}>
+                                        <p className={`text-xl drop-shadow-2xl font-bold font-inconsolata text-light-color group-hover:scale-125 duration-100`}>{project.name}</p>
+                                    </div>
+                                </a>
                             </picture>
-                        )
-                    })}
+                        ))
+                    )}
                 </div>
             </div>
             <div className={`grid w-full gap-4 overflow-hidden duration-200 ${showSkills ? "open" : "close"}`}>
                 <div
-                    className={`flex flex-col items-center w-full h-fit gap-4 overflow-hidden larger-screen:flex-row larger-screen:flex-wrap larger-screen:gap-8`}>
-                    {info && info.skills.map((project, id) => {
+                    className={`flex flex-wrap justify-center items-center w-full h-fit gap-4 overflow-hidden larger-screen:justify-start larger-screen:gap-8`}>
+                    {info && info.skills.map((skill, id) => {
                         return (
-                            <picture key={id}
-                                     className={`overflow-hidden flex items-center justify-center w-52 h-36 bg-primary-button rounded-lg cursor-pointer larger-screen:w-[300px] larger-screen:h-[210px]`}>
-                                <img src="./vite.svg" alt="image"
-                                     className={`size-20 object-cover larger-screen:size-40 select-none`}/>
-                            </picture>
+                            <div key={id}
+                                 className={`overflow-hidden flex items-center justify-center size-fit  rounded-lg cursor-pointer`}>
+                                <picture
+                                    className={`size-40 flex flex-col items-center justify-center gap-4 bg-dark-color-second p-2`}>
+                                    <img src={skill.image} alt={skill.name}
+                                         className={`size-16 select-none rounded`}/>
+                                    <p className={`text-lg font-inconsolata text-light-color`}>{skill.name}</p>
+                                </picture>
+                            </div>
                         )
                     })}
                 </div>
