8f1dc4ca1e8b2ef63c1a3b5a61986c2f889ef123
Davi Santanna <davicalvalcanti@gmail.com>
2024-05-22 21:25:12 -0300

    FEAT: "add new AboutFrontEndFusion component"

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-
-import './App.css'
+import "./App.css";
 import EquipeCollection from "./components/equipe.collection";
-import ComentariosContainer from './components/comentarios.collection'
-import React from 'react'
-
-
+import ComentariosContainer from "./components/comentarios.collection";
+import AboutFrontEndFusion from "./components/sobre-o-projeto.collection";
+("./components/sobre-o-projeto.collection/");
+import React from "react";
 
 function App() {
-
   return (
-<div className=' flex flex-col gap-10'>
-
-<span>[Coloque aqui o hero]</span>
-<span>[Coloque aqui a sessão de vantagens]</span>
-<span>[Coloque aqui a sesão de tecnologias]</span>
-<EquipeCollection />
-<ComentariosContainer />
-<span>[Coloque aqui a sessão de call to action]</span>
-<span>[Coloque aqui o cabecalho]</span>
-
-</div>
-  )
+    <div className=" flex flex-col gap-10">
+      <span>[Coloque aqui o hero]</span>
+      <span>[Coloque aqui a sessão de vantagens]</span>
+      <span>[Coloque aqui a sesão de tecnologias]</span>
+      <EquipeCollection />
+      <AboutFrontEndFusion />
+      <ComentariosContainer />
+      <span>[Coloque aqui o cabecalho]</span>
+    </div>
+  );
 }
 
-export default App
+export default App;
