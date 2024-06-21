ad29d0d1978585c1720e330a76539e9623127d1a
Filipe Dias <diasjoaum@gmail.com>
2024-06-21 16:30:18 -0300

    Implementação do sistema backend feita

--- src/components/equipe.collection/indexComponents/EquipeBottom.jsx
+++ src/components/equipe.collection/indexComponents/EquipeBottom.jsx
@@ -1,22 +1,10 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import projectDefault from "../imageProject/projectDefault.png";
 
 export default function EquipeBottom(User) {
     const [showProject, setShowProject] = useState(false)
     const [showSkills, setShowSkills] = useState(false)
-
-    const [info, setInfo] = useState()
-    const {user, infos} = User
-
-    useEffect(() => {
-        for (const name in infos) {
-            if (name.toLowerCase() === user.login.toLowerCase()) {
-                setInfo(infos[name])
-                break
-            }
-        }
-    }, [infos])
-
+    const {user} = User
 
     function toggleProjects() {
         setShowProject(!showProject)
@@ -29,7 +17,7 @@ export default function EquipeBottom(User) {
     }
 
     return (
-        <div className={`space-y-4 w-full h-fit mt-4 larger-screen:mt-16`}>
+        <div className={`space-y-4 w-full h-fit mt-4 larger-screen:mt-16 larger-screen:space-y-0`}>
             <menu
                 className={`flex flex-wrap justify-center w-full h-fit gap-2 larger-screen:justify-start larger-screen:gap-8 larger-screen:mb-8`}>
                 <button onClick={toggleProjects}
@@ -44,7 +32,7 @@ export default function EquipeBottom(User) {
             <div className={`grid w-full gap-4 overflow-hidden duration-200 ${showProject ? "open" : "close"}`}>
                 <div
                     className={`flex items-center justify-center w-full h-fit gap-4 overflow-hidden larger-screen:flex-row larger-screen:justify-start flex-wrap larger-screen:gap-8`}>
-                    {info && info.projects.length === 0 ? (
+                    {user.projects.length === 0 ? (
                         <div
                             className="flex items-center justify-center flex-col gap-2 w-full h-fit larger-screen:items-start">
                             <p className={`font-inconsolata font-bold text-center dark:text-light-color text-3xl`}>
@@ -52,7 +40,7 @@ export default function EquipeBottom(User) {
                             </p>
                         </div>
                     ) : (
-                        info && info.projects.map((project, id) => (
+                        user.projects.map((project, id) => (
                             <picture key={id}
                                      className={`group overflow-hidden flex items-center justify-center w-52 h-36 dark:bg-dark-color-second rounded-lg cursor-pointer larger-screen:w-[300px] larger-screen:h-[210px]`}>
                                 <a href={project.link} target="_blank"
@@ -72,21 +60,30 @@ export default function EquipeBottom(User) {
             <div className={`grid w-full gap-4 overflow-hidden duration-200 ${showSkills ? "open" : "close"}`}>
                 <div
                     className={`flex flex-wrap justify-center items-center w-full h-fit gap-4 overflow-hidden larger-screen:justify-start larger-screen:gap-8`}>
-                    {info && info.skills.map((skill, id) => {
-                        return (
-                            <div key={id}
-                                 className={`overflow-hidden flex items-center justify-center size-fit rounded-lg cursor-pointer`}>
-                                <picture
-                                    className={`size-40 flex flex-col items-center justify-center gap-4 bg-[#111111] dark:bg-dark-color-second p-2`}>
-                                    <img src={skill.image} alt={skill.name}
-                                         className={`size-16 select-none rounded`}/>
-                                    <p className={`text-lg font-inconsolata text-light-color`}>{skill.name}</p>
-                                </picture>
-                            </div>
-                        )
-                    })}
+                    <div
+                        className="flex items-center justify-center flex-col gap-2 w-full h-fit larger-screen:items-start">
+                        <p className={`font-inconsolata font-bold text-center dark:text-light-color text-3xl`}>
+                            Nenhuma habilidade adicionada.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
+
+
+// Na espera da parte de habilidades ser implementada no sistema banckend
+// {info && info.skills.map((skill, id) => {
+//     return (
+//         <div key={id}
+//              className={`overflow-hidden flex items-center justify-center size-fit rounded-lg cursor-pointer`}>
+//             <picture
+//                 className={`size-40 flex flex-col items-center justify-center gap-4 bg-[#111111] dark:bg-dark-color-second p-2`}>
+//                 <img src={skill.image} alt={skill.name}
+//                      className={`size-16 select-none rounded`}/>
+//                 <p className={`text-lg font-inconsolata text-light-color`}>{skill.name}</p>
+//             </picture>
+//         </div>
+//     )
+// })}
